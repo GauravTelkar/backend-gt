4e1f7d784192654d2e982c91d9e9522e60efee87
gaurav.telkar <gaurav.telkar@collabera.com>
2022-01-04 04:39:59 -0800

    Modified : learning inheritance along with unit testing

--- 1stWeek/2nd-Day/java-simple-app/src/test/java/BmwTests.java
+++ 1stWeek/2nd-Day/java-simple-app/src/test/java/BmwTests.java
@@ -1,4 +1,5 @@
 import clasobj.Bmw;
+import clasobj.Car;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,5 +19,21 @@ public class BmwTests {
         int expectedSpeed=10;
         int actualSpeed= bmw.speed;
         Assertions.assertEquals(expectedSpeed,actualSpeed);
+    }
+    @DisplayName("test is bmw is a car")
+    @Test
+    void testBmwObjectCar(){
+       Bmw bmw=new Bmw();
+       boolean expected =true;
+       boolean actual = bmw instanceof Car;
+       Assertions.assertEquals(expected, actual);
+    }
+    @DisplayName("Test car is not a bmw")
+    @Test
+    void testCarObjectNotBmw(){
+        Car car = new Car();
+        boolean expected = false;
+        boolean actual = car instanceof Bmw;
+        Assertions.assertEquals(expected, actual);
     }
 }
