36c7ea0ddbdb467f315d38f43365a70358b47d19
gaurav.telkar <gaurav.telkar@collabera.com>
2022-01-04 21:20:49 -0800

    dividing two number : 1

---
+++ 1stWeek/day-3/java-other-concept/src/main/java/oop/Divid.java
@@ -0,0 +1,9 @@
+package oop;
+
+public class Divid {
+    public int divided(int num1,int num2)
+    {
+        int num= num1/num2;
+        return num;
+    }
+}
---
+++ 1stWeek/day-3/java-other-concept/src/test/java/DividingTwoNumberTests.java
@@ -0,0 +1,23 @@
+import oop.Divid;
+import oop.DividingTwoNumber;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.Test;
+
+public class DividingTwoNumberTests extends DividingTwoNumber {
+
+    @Test
+    public void testNotNull()
+    {
+        DividingTwoNumber div=new DividingTwoNumber();
+        Assertions.assertNotNull(div);
+    }
+    @Test
+    void testForTwoNum()
+    {
+        Divid div=new Divid();
+        double expected = 2;
+        double actual= div.divided(4,2);
+        Assertions.assertEquals(expected,actual);
+    }
+}
