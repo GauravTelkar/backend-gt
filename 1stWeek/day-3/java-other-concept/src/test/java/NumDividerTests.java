b6a20f1cfc3644b3cdd60c42a95d4f5d2f0b187b
gaurav.telkar <gaurav.telkar@collabera.com>
2022-01-04 22:10:35 -0800

    dividing two number : modified with exception handler

---
+++ 1stWeek/day-3/java-other-concept/src/test/java/NumDividerTests.java
@@ -0,0 +1,52 @@
+import exception.NumDivider;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class NumDividerTests {
+    @DisplayName("test for complete numbers")
+    @Test
+    void testCompleteNumbers() {
+        var dvDr = new NumDivider();
+        var num1 = 10;
+        var num2 = 10;
+        var expected = 1;
+        var actual = dvDr.divideNumbers(num1, num2);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @DisplayName("Test for precision numbers")
+    @Test
+    void testPrecisionNumbers() {
+        var dvDr = new NumDivider();
+        var num1 = 4.2;
+        var num2 = 2.2;
+        var expected = 1.9090;
+        var actual = dvDr.divideNumbers(num1, num2);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @DisplayName("Test for Unexpected Numbers")
+    @Test
+    void testUnexpectedNumbers() {
+        var num1 = 4.5;
+        var num2 = 0;
+
+        var dvDr = new NumDivider();
+        var expected = 0;
+        var actual = dvDr.divideNumbers(num1, num2);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @DisplayName("Testing Exceptions")
+    @Test
+    void testUnexpectedNumbersWithException() {
+        var num1 = 4.5;
+        var num2 = 0;
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            var dvDr = new NumDivider();
+            dvDr.divideNumbersException(num1, num2);
+        });
+    }
+}
