97aa4ca9b3382a763fbed0216e6e3a2d54f9771b
gaurav.telkar <gaurav.telkar@collabera.com>
2022-01-05 02:34:07 -0800

    implementing collection demos : modified with map

--- 1stWeek/day-3/java-other-concept/src/main/java/collections/CollectionDemo.java
+++ 1stWeek/day-3/java-other-concept/src/main/java/collections/CollectionDemo.java
@@ -5,7 +5,17 @@ import java.util.function.Consumer;
 public class CollectionDemo {
     public static void main(String[] args) {
         CollectionDemo cdm = new CollectionDemo();
+        System.out.println("----- List Demo -----");
+        cdm.listDemo();
+        System.out.println("----- Set Demo -----");
+        cdm.setDemo();
+        System.out.println("----- list Demo -----");
+        cdm.listDemo();
+        System.out.println("------------Queue-----------");
         cdm.queueDemo();
+
+        System.out.println("----- Map Demo -----");
+        cdm.mapDemo();
     }
 
     public List<String> listDemo() {
@@ -54,5 +64,26 @@ public class CollectionDemo {
 
         queue.forEach(System.out::println);
         return queue;
+    }
+    public void mapDemo() {
+
+        Map<Integer, String> map = new HashMap<Integer, String>();
+        map.put(1, "abc");
+        map.put(2, "pqr");
+        map.put(3, "lmn");
+        map.put(4, "xyz");
+
+        Set<Integer> keys =  map.keySet();
+        keys.forEach( key -> System.out.println("Key - "+key) );
+
+        Collection<String> values = map.values();
+        values.forEach( val -> System.out.println("Value - "+val) );
+
+        map.remove(1, "abc");
+        System.out.println("2 available -> "+map.containsKey(2));;
+        System.out.println("lmn available -> "+map.containsValue("lmn"));;
+        System.out.println("Element At 3 -> "+ map.getOrDefault(3, "none"));
+
+        map.forEach( (k, v) -> System.out.println("Key - "+k +" Value - "+v) );
     }
 }
