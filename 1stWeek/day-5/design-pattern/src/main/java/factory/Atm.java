67185053d8a2a78ef6b36cb2d75ca6da8c81e84c
gaurav.telkar <gaurav.telkar@collabera.com>
2022-01-07 01:30:34 -0800

    factory : card and coin

---
+++ 1stWeek/day-5/design-pattern/src/main/java/factory/Atm.java
@@ -0,0 +1,9 @@
+package factory;
+
+public class Atm {
+    public CardProvider swipe(String bank) {
+        CardProvider card = CardFactory.getCard(bank);
+        card.swipe(150);
+        return card;
+    }
+}
---
+++ 1stWeek/day-5/design-pattern/src/main/java/factory/CardFactory.java
@@ -0,0 +1,18 @@
+package factory;
+
+public class CardFactory extends Factory{
+
+    private CardFactory() { }
+
+    public static CardFactory newInstance() {
+        return new CardFactory();
+    }
+
+    public static CardProvider getCard(String bank) {
+        if(bank.equals("pqr")) return new PqrCard();
+        if(bank.equals("abc")) return new AbcCard();
+        if(bank.equals("xyz")) return new XyzCard();
+        throw new InvalidBankException("ATM Card Facility is not provided by "+bank);
+    }
+
+}
