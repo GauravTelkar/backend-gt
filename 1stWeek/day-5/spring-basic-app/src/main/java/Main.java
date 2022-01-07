fecdfebdd5d658a8f0f29d8dda8898bb602ccad2
gaurav.telkar <gaurav.telkar@collabera.com>
2022-01-07 03:51:56 -0800

    spring : dependecy injection

---
+++ 1stWeek/day-5/spring-basic-app/src/main/java/Main.java
@@ -0,0 +1,15 @@
+import car.Car;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Car car1 = (Car) context.getBean("carakfjhlskfhslhlshg");
+        car1.setSpeed(100);
+
+        Class<Car> cls = Car.class; // this is not the bytecode class file
+        Car car2 = context.getBean(cls);
+        System.out.println("Car Speed is "+ car2.getSpeed());
+    }
+}
---
+++ 1stWeek/day-5/spring-basic-app/src/main/java/car/Car.java
@@ -0,0 +1,13 @@
+package car;
+
+public class Car {
+    private int speed;
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+}
