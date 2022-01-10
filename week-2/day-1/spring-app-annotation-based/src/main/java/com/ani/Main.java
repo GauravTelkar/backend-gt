b62f15b24ff870fb670bffff4474500a855fdfc9
gaurav.telkar <gaurav.telkar@collabera.com>
2022-01-09 22:07:11 -0800

    spring core : annotation based config

---
+++ week-2/day-1/spring-app-annotation-based/src/main/java/com/ani/Main.java
@@ -0,0 +1,18 @@
+package com.ani;
+
+import com.ani.car.Car;
+import com.ani.config.AppConfig;
+import com.ani.driver.Driver;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class); // hey spring - i have written beans configuration in AppConfig class
+        Car car1= (Car) context.getBean("car");
+        Driver driver1 = (Driver) context.getBean("driver");
+        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+    }
+}
---
+++ week-2/day-1/spring-app-annotation-based/src/main/java/com/ani/config/AppConfig.java
@@ -0,0 +1,20 @@
+package com.ani.config;
+
+import com.ani.car.Car;
+import com.ani.driver.Driver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("com.ani")
+@Configuration
+public class AppConfig {
+    @Bean
+    public Car car(){
+        return new Car();
+    }
+    @Bean
+    public Driver driver(){
+        return new Driver();
+    }
+}
