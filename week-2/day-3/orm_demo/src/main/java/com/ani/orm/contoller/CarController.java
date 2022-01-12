17c5a6da24d28148e6574710fe0f3300c223b68e
gaurav.telkar <gaurav.telkar@collabera.com>
2022-01-12 03:53:00 -0800

    spring data basic setup

---
+++ week-2/day-3/orm_demo/src/main/java/com/ani/orm/contoller/CarController.java
@@ -0,0 +1,23 @@
+package com.ani.orm.contoller;
+
+import com.ani.orm.domain.Car;
+import com.ani.orm.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/car")
+@RestController
+public class CarController {
+
+    @Autowired
+    private CarService service;
+
+    @PostMapping
+    private String saveCar(@RequestBody Car car){
+        service.saveCar(car);
+        return "car saved sucessfully";
+    }
+}
