27333e8ecb69303e159890805b856900374e834f
gaurav.telkar <gaurav.telkar@collabera.com>
2022-01-13 04:14:42 -0800

    spring security : basic authentication

---
+++ week-2/day-4/basic-auth/src/test/java/com/ani/basic/auth/BasicAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ani.basic.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
