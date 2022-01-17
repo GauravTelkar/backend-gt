c62618e342450b7a99886b7eefb213c04392f297
gaurav.telkar <gaurav.telkar@collabera.com>
2022-01-17 00:39:12 -0800

    microservice

---
+++ week-3/day-1/microservice-basics/service-register/src/test/java/com/ani/register/service/ServiceRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ani.register.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
